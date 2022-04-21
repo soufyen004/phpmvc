b9d78599a52e6fba05c985b8888796bb8df4740b
soufyen <soufyen004@gmail.com>
2022-04-21 02:20:32 +0000

    this is first commit

---
+++ app/configs/config.php
@@ -0,0 +1,15 @@
+
+<?php
+//app link
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://localhost/php_MVC/');
+define('APPNAME', 'Sharepost using oop php mvc');
+
+
+
+//create database info
+define("DB_HOST", "localhost");
+define("DB_USER", 'root');
+define("DB_PASSWORD", '');
+define("DB_NAME", 'sharepost');

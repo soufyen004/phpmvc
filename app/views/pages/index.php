69b8ad12545385ef5c6a0d409065e3fc8b1d8fc9
soufyen <soufyen004@gmail.com>
2022-04-24 16:50:03 +0000

    views optimized

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,6 +1,5 @@
 <?php include_once APPROOT . '/views/inc/header.inc.php'; ?>
 
-
 <div class="jumbotron p-4 mt-5 mx-auto">
     <h1>This application is a blogger app that let u add and remove and edit or view ur Posts or other users posts</h1>
 </div>
